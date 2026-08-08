32dbbd919fd91bf056dd711d08d76765b8979e69
Acid3croco <acid3croco@users.noreply.github.com>
2026-08-08 20:12:19 +0000

    tests : aligner les factices sur la table des effets

--- tests/criteria_test.py
+++ tests/criteria_test.py
@@ -81,9 +81,9 @@ class FakeConn:
         if "FROM work_item w" in sql:
             return FakeCursor(self.items)
         if sql.startswith("INSERT INTO effect"):
-            self.effects.setdefault((params[2], params[1]), "not_attempted")
+            self.effects.setdefault((params[3], params[2]), "not_attempted")
             return FakeCursor([])
-        if sql.startswith("SELECT observation FROM effect"):
+        if sql.startswith("SELECT * FROM effect"):
             return FakeCursor([{"observation": self.effects.get((params[0], params[1]))}])
         if sql.startswith("UPDATE effect SET observation"):
             if not self.amnesique:
